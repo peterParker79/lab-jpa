4ce4233fe3c1e12f7b27de41e6fbf1946e7153ea
peterParker79 <trypeterparker@gmail.com>
2025-04-16 16:13:21 +0200

    Class Person

---
+++ demo/src/main/java/com/example/demo/models/Person.java
@@ -0,0 +1,53 @@
+package com.example.demo.models;
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name="person")
+public class Person {
+    @Id // Indica a nuestra app que es una PRIMARY key
+    @GeneratedValue(strategy=GenerationType.IDENTITY)//Notación para indicar que es autoincremental
+    private int id;
+
+    @Column(name ="name")
+    String name;
+
+    @Column(name = "age")
+    int age;
+
+
+    public Person() {} // Necesario el contructor vacio
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
